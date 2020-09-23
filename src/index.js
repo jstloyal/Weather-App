78036706461744c2323174c4a252b5e8853dc19b
jstloyal <jstloyalty@gmail.com>
2020-09-23 12:54:19 +0100

    Add html boilerplate

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './assets/style.scss';
+import './assets/styles.scss';
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
